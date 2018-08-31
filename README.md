1fafa53a82038b6d6578f254b9da7de9854d551b
Erick Wendel <erick.workspace@gmail.com>
2018-08-31 15:56:36 -0300

    added concurrently task
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- README.md
+++ README.md
@@ -4,12 +4,16 @@
 
 ### Requirements
 
-* Node.js v7+
-* MongoDB running on local instance
+- Node.js v7+
+- MongoDB running on local instance
 
 ### Running
 
-* Install dependencies - `npm i`
-* Build typescript - `npm run build`
-* Run project - `npm start`
-* Go to swagger page - `localhost:3000/documentation`
+- Install dependencies - `npm i`
+- Build typescript - `npm run build`
+- Run project - `npm start`
+- Go to swagger page - `localhost:3000/documentation`
+
+### Development with Watch Compiler
+
+- Run project - `npm run dev`
--- index.ts
+++ index.ts
@@ -1,20 +1,20 @@
-import * as Hapi from "hapi";
-import * as Joi from "joi";
-import { MongoClient } from "mongodb";
+import * as Hapi from 'hapi';
+import * as Joi from 'joi';
+import { MongoClient } from 'mongodb';
 
-const Inert = require("inert");
-const Vision = require("vision");
-const HapiSwagger = require("hapi-swagger");
+const Inert = require('inert');
+const Vision = require('vision');
+const HapiSwagger = require('hapi-swagger');
 const server = new Hapi.Server();
 const port = process.env.PORT || 3000;
 server.connection({ port });
 
 (async () => {
   const connectionString = `mongodb://${process.env.MONGO_URL ||
-    "localhost"}/heroes`;
+    'localhost'}/heroes`;
   const connection = await MongoClient.connect(connectionString);
-  console.log("mongo db is running");
-  const db = connection.db("heroes").collection("hero");
+  console.log('mongo db is running');
+  const db = connection.db('heroes').collection('hero');
   await server.register([
     Inert,
     Vision,
@@ -22,65 +22,65 @@ server.connection({ port });
       register: HapiSwagger,
       options: {
         info: {
-          title: "Node.js with MongoDB Example - Erick Wendel",
-          version: "1.0"
-        }
-      }
-    }
+          title: 'Node.js with MongoDB Example - Erick Wendel',
+          version: '1.0',
+        },
+      },
+    },
   ]);
 
   server.route([
     {
-      method: "GET",
-      path: "/heroes",
+      method: 'GET',
+      path: '/heroes',
       config: {
         handler: (req: any, reply: any) => {
           return reply(db.find().toArray());
         },
-        description: "List All heroes",
-        notes: "heroes from database",
-        tags: ["api"]
-      }
+        description: 'List All heroes',
+        notes: 'heroes from database',
+        tags: ['api'],
+      },
     },
     {
-      method: "POST",
-      path: "/heroes",
+      method: 'POST',
+      path: '/heroes',
       config: {
         handler: (req, reply) => {
           const { payload } = req;
           return reply(db.insert(payload));
         },
-        description: "Create a hero",
-        notes: "create a hero",
-        tags: ["api"],
+        description: 'Create a hero',
+        notes: 'create a hero',
+        tags: ['api'],
         validate: {
           payload: {
             name: Joi.string().required(),
-            power: Joi.string().required()
-          }
-        }
-      }
+            power: Joi.string().required(),
+          },
+        },
+      },
     },
 
     {
-      method: "DELETE",
-      path: "/heroes/{id}",
+      method: 'DELETE',
+      path: '/heroes/{id}',
       config: {
         handler: (req, reply) => {
           return reply(db.remove({ _id: req.params.id }));
         },
-        description: "Delete a hero",
-        notes: "Delete a hero",
-        tags: ["api"],
+        description: 'Delete a hero',
+        notes: 'Delete a hero',
+        tags: ['api'],
         validate: {
           params: {
-            id: Joi.string().required()
-          }
-        }
-      }
-    }
+            id: Joi.string().required(),
+          },
+        },
+      },
+    },
   ]);
 
   await server.start();
-  console.log("server running at", port);
+  console.log('server running at', port);
 })();
